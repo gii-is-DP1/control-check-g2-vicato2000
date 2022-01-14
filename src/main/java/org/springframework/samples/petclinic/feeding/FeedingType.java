4a67e4b495fa8d068db350ab8722937a3b4d0e68
vicato2000 <vicentect10@gmail.com>
2022-01-14 09:46:25 +0100

    Control check lab Vicente Cambrón Tocados

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
@@ -1,15 +1,32 @@
 package org.springframework.samples.petclinic.feeding;
 
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
-public class FeedingType {
-    Integer id;
+@Entity
+@Table(name = "feedings_type")
+public class FeedingType extends BaseEntity {
+
+    @Size(min = 3, max = 30)
+    @NotEmpty
+    @Column(name = "name", unique = true)
     String name;
+
+    @NotEmpty
     String description;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "id", name = "pet_type_id")
     PetType petType;
 }
